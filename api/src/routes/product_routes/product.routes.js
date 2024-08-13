78f3374fce3f85d373d892dde8c0ca0d46725e46
akki83340876 <133499635+akki83340876@users.noreply.github.com>
2024-08-13 16:02:53 +0530

    Update product.routes.js

--- api/src/routes/product_routes/product.routes.js
+++ api/src/routes/product_routes/product.routes.js
@@ -5,5 +5,7 @@ const {add_product,get_product } = require("../../controller/product_controller/
 
 router.post('/create_product',add_product)
 router.get('/get_all_products',get_product)
+router.patch('/update_product',update_product)
+router.delete('/delete_product/:id',delete_product)
 
 module.exports = router
